039c1d8845a2a3c70e88d0c924493f647b4eab56
wafijailani <wafijailani10@live.com>
2022-12-28 00:03:31 +0800

    updated

--- water-selangor-scrape.py
+++ water-selangor-scrape.py
@@ -106,37 +106,41 @@ engine = create_engine("mysql+pymysql://root:@localhost/myfloodlaravel")
 df.to_sql('selangorscrape', con = engine, if_exists='replace')
 
 
-
-
 for i, row in df.iterrows():
 	if row['smsAlert'] == 'sms':
 		x = df['District'][i]
 		print(x)
 		newformat = ("{}\n{}\n{}\n{}\n{}".format('Alert Warning from Myflood (Alert)',df['Station Name'][i], df['CurrentWaterLevel'][i], df['Last Update'][i],'Check Website for More Information'))
-		concursor.execute("SELECT phonenum FROM users WHERE district = '{x}'".format(x=x))
+		concursor.execute("SELECT phonenum FROM users WHERE status = 1 AND district = '{x}'".format(x=x))
 		result = concursor.fetchall()
 		print(result)
-		for i in range(len(result)):
-			message = client.messages.create(messaging_service_sid='MG2815ed8745527fe1d9366bdf1cbbfc4d', body=newformat ,to=result[i])
+		# for i in range(len(result)):
+		# 	message = client.messages.create(messaging_service_sid='MG2815ed8745527fe1d9366bdf1cbbfc4d', body=newformat ,to=result[i])
 
 for i, row in df.iterrows():
 	if row['smsWarning'] == 'sms':
 		x = df['District'][i]
 		print(x)
 		newformat = ("{}\n{}\n{}\n{}\n{}".format('Alert Warning from Myflood (Warning)',df['Station Name'][i], df['CurrentWaterLevel'][i], df['Last Update'][i],'Check Website for More Information'))
-		concursor.execute("SELECT phonenum FROM users WHERE district = '{x}'".format(x=x))
+		concursor.execute("SELECT phonenum FROM users WHERE status = 1 AND district = '{x}'".format(x=x))
 		result = concursor.fetchall()
 		print(result)
-		for i in range(len(result)):
-			message = client.messages.create(messaging_service_sid='MG2815ed8745527fe1d9366bdf1cbbfc4d', body=newformat ,to=result[i])
+		# for i in range(len(result)):
+		# 	message = client.messages.create(messaging_service_sid='MG2815ed8745527fe1d9366bdf1cbbfc4d', body=newformat ,to=result[i])
 
 for i, row in df.iterrows():
 	if row['smsDanger'] == 'sms':
 		x = df['District'][i]
 		print(x)
 		newformat = ("{}\n{}\n{}\n{}\n{}".format('Alert Warning from Myflood (Danger)',df['Station Name'][i], df['CurrentWaterLevel'][i], df['Last Update'][i],'Check Website for More Information'))
-		concursor.execute("SELECT phonenum FROM users WHERE district = '{x}'".format(x=x))
+		concursor.execute("SELECT phonenum FROM users WHERE status = 1 AND district = '{x}'".format(x=x))
 		result = concursor.fetchall()
 		print(result)
-		for i in range(len(result)):
-			message = client.messages.create(messaging_service_sid='MG2815ed8745527fe1d9366bdf1cbbfc4d', body=newformat ,to=result[i])
+		# for i in range(len(result)):
+		# 	message = client.messages.create(messaging_service_sid='MG2815ed8745527fe1d9366bdf1cbbfc4d', body=newformat ,to=result[i])
+
+
+
+
+
+		concursor.execute("SELECT phonenum FROM users WHERE status = 1 AND district = '{x}'".format(x=x))
